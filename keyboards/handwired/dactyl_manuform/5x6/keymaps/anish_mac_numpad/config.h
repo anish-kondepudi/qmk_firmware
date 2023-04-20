373d057f8402ae36c5ad84496b1cd0a969c21ab3
Anish Kondepudi <anish.kondepudi@gmail.com>
2023-04-19 22:19:50 -0700

    Add anish numpad mac keymap

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/anish_mac_numpad/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/anish_mac_numpad/config.h
@@ -1,6 +1,5 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,32 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
+#define MASTER_LEFT
+// #define MASTER_RIGHT
 
-/* key matrix size */
-// Rows are doubled-up
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 6
-
-// wiring of each half
-// #define MATRIX_COL_PINS { D7, C6, D4, E6, B4, B5 } //right
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 } //left
-#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3 }
-
-#define DIODE_DIRECTION COL2ROW
-
-// WS2812 RGB LED strip input and number of LEDs
-//#define RGB_DI_PIN D3
-#define RGBLED_NUM 12
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* number of backlight levels */
-// #define BACKLIGHT_LEVELS 3
-
-//#define AUDIO_PIN B6
-
-#define TAPPING_TOGGLE 2
-
-#define USE_SERIAL
+//#define EE_HANDS
